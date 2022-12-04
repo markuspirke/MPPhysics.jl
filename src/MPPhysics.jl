df5feae60684f3367e757537ddfdbc690ac81c41
Markus Pirke <markus.pirke@web.de>
2022-12-04 11:05:45 +0100

    set foo... file to ignore in include

--- src/MPPhysics.jl
+++ src/MPPhysics.jl
@@ -8,6 +8,7 @@ export
 # Write your package code here.
 for inc ∈ readdir(joinpath(@__DIR__, "mechanics"), join=true)
     !endswith(inc, ".jl") && continue
+    startswith(split(inc, "/")[end], "foo") && continue
     include(inc)
 end
 end
