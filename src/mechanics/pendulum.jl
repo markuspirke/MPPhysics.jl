5f316838a07518fb1b70499d81a327e19cbb44fc
Markus Pirke <markus.pirke@web.de>
2022-12-07 09:50:48 +0100

    added documentary to functions in pendulum.jl

--- src/mechanics/pendulum.jl
+++ src/mechanics/pendulum.jl
@@ -1,3 +1,8 @@
+"""
+    struct Pendulum
+
+This type stores all the parameters and start values of a simple ideal pendulum.
+"""
 struct Pendulum
     g
     m
@@ -5,18 +10,31 @@ struct Pendulum
     ϕ₀
     dϕ₀
 end
+"""
+    function (pen::Pendulum)(p, q, param)
 
+Hamiltonian of a simple pendulum.
+"""
 function (pen::Pendulum)(p, q, param)
     H = p^2 / (2 * pen.m * (pen.l)^2) - pen.m * pen.g * pen.l * cos(q)
     H
 end
+"""
+    function startvalues(pen::Pendulum)
 
+Returns the starting values of a pendulum as p, q. 
+"""
 function startvalues(pen::Pendulum)
     q₀ = pen.ϕ₀
     p₀ = pen.dϕ₀ * pen.m * (pen.l)^2
     p₀, q₀
 end
+"""
+    function trajectory(pen::Pendulum, tspan; kwargs...)
 
+Calculates the trajectory of a simple pendulum.
+Returns the times t, generalized momenta ps and generalized position qs.
+"""
 function trajectory(pen::Pendulum, tspan; kwargs...)
     p₀, q₀ = startvalues(pen)
     prob = HamiltonianProblem(pen, p₀, q₀, tspan)
@@ -28,14 +46,22 @@ function trajectory(pen::Pendulum, tspan; kwargs...)
 
     t, ps, qs
 end
+"""
+    function integrate(pen::Pendulum, tspan; kwargs...)
 
+Sets up time stepping for the pendulum.
+"""
 function integrate(pen::Pendulum, tspan; kwargs...)
     p₀, q₀ = startvalues(pen)
     prob = HamiltonianProblem(pen, p₀, q₀, tspan)
     init(prob, Tsit5(); kwargs...)
 end
 
+"""
+    function cartesian(q, pen::Pendulum)
 
+Returns cartesian coordinates of the generalized coordinates.
+"""
 function cartesian(q, pen::Pendulum)
     y, x = polarcoordinates(q, pen.l)
     y = -y # because of defintion of the angle
