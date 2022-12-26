bc193e25db48073045e011aacea8e046ac5101c9
Markus Pirke <markus.pirke@web.de>
2022-12-26 22:11:40 +0100

    changed a view tests

--- test/pendulum.jl
+++ test/pendulum.jl
@@ -5,11 +5,11 @@ using Test
     pen = Pendulum(9.81, 1.0, 1.0, 0.0, 0.0)
     p, q = startvalues(pen)
     @test -9.81 ≈ pen(p, q, nothing)
-    x, y = cartesian(q, pen)
+    x, y = cartesian(pen, q)
     @test 0.0 == x
     @test -1.0 == y
     pen = Pendulum(9.81, 1.0, 1.0, pi / 4, 0.0)
-    x, y = cartesian(pen.ϕ₀, pen)
+    x, y = cartesian(pen, pen.ϕ₀)
     @test 1 / sqrt(2) ≈ x
     @test -1 / sqrt(2) ≈ y
 
--- test/runtests.jl
+++ test/runtests.jl
@@ -4,4 +4,6 @@ using Test
 @testset "MPPhysics.jl" begin
     include("doublependulum.jl")
     include("coordinates.jl")
+    include("pendulum.jl")
+    include("moving_pendulum.jl")
 end
