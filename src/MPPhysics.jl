ac787ed3cac22b64c11d5a46e327864e21ca2df6
Markus Pirke <markus.pirke@web.de>
2022-12-26 22:05:46 +0100

    added new dependencies

--- src/MPPhysics.jl
+++ src/MPPhysics.jl
@@ -1,16 +1,26 @@
 module MPPhysics
 using DifferentialEquations
 using DiffEqPhysics
+using StaticArrays
+using LinearAlgebra
+using AngleBetweenVectors
 export
+    Position2D, Position, SO2,
     HamiltonianSystem, integrate, trajectory,
     polarcoordinates,
     simulate, cartesian,
     Pendulum, startvalues,
-    DoublePendulum
+    DoublePendulum,
+    Spring, SpringMass,
+    MovingPendulum, lagrangian
+
 # Write your package code here.
+include("types.jl")
+
 for inc ∈ readdir(joinpath(@__DIR__, "mechanics"), join=true)
     !endswith(inc, ".jl") && continue
     startswith(split(inc, "/")[end], "foo") && continue
     include(inc)
 end
+
 end
